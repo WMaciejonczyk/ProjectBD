7e020ca41ba11985f5a704e2541f7f980876b190
WMaciejonczyk <147508482+WMaciejonczyk@users.noreply.github.com>
2024-01-27 15:10:04 +0100

    Adding database file

---
+++ src/main/java/database/database.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS medical_equipment;
+
+CREATE DATABASE medical_equipment;
+
+USE medical_equipment;
+
+CREATE TABLE info(
+eq_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+eq_name VARCHAR(255) NOT NULL,
+eq_type ENUM('ECG', 'EMG', 'RTG', 'MRI', 'other'),
+production_date DATE NOT NULL,
+last_service_date DATE NOT NULL,
+eq_status ENUM('free', 'occupied') NOT NULL,
+service_validity_period SMALLINT NOT NULL
+);
+
+CREATE TABLE service_archives(
+service_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+service_date DATE NOT NULL,
+equipment_id INT NOT NULL,
+technician VARCHAR(255) NOT NULL,
+description TEXT NOT NULL,
+FOREIGN KEY (equipment_id) REFERENCES info(eq_id)
+);
+
+CREATE TABLE reservations(
+reservation_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+start_date DATE NOT NULL,
+end_date DATE NOT NULL,
+equipment_id INT NOT NULL,
+reserver VARCHAR(255) NOT NULL,
+FOREIGN KEY (equipment_id) REFERENCES info(eq_id)
+);
+
+CREATE TABLE users(
+user_login VARCHAR(30) PRIMARY KEY NOT NULL,
+user_password VARCHAR(30) NOT NULL,
+department ENUM('doctor', 'technician', 'admin') NOT NULL
+);
+
+INSERT INTO users(user_login, user_password, department)
+ VALUES('project_admin', '1234', 'admin');
+ 
+SELECT * FROM users;
