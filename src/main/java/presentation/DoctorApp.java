a09b607d92002df387eafe7db149ac8189cdf103
MateuszLickindorf <268389@student.pwr.edu.pl>
2024-02-02 13:17:59 +0100

    Improved Visualizing Options

--- src/main/java/presentation/DoctorApp.java
+++ src/main/java/presentation/DoctorApp.java
@@ -1,19 +1,16 @@
 package presentation;
 
 
-import persistance.archives.IArchives;
 import persistance.entity.InfoEntity;
+import persistance.entity.ReservationsEntity;
 import persistance.equipment.IEquipmentStorage;
 import persistance.reservations.IReservationsRepository;
 import persistance.view.DoctorViewStorage;
 import service.*;
-import types.EqStatus;
-import types.EqType;
+
 import java.sql.Date;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class DoctorApp {
 
@@ -58,23 +55,32 @@ public class DoctorApp {
         switch (option) {
             case "1":
             case "Reserve equipment":
-                startReservation();
+                //startReservation();
                 break;
             case "2":
             case "Change equipment usage status":
-                reportService();
+                // !!
                 break;
             case "3":
             case "Show a list of equipment (with status)":
-                List<InfoEntity> AvailableItems = doc.getAvailableDevices(storage);
-                System.out.println("Name  Type   Status");
-                for (DoctorViewItem item : items) {
-                    System.out.println(item.getName() + "   " + item.getType() + "  " + item.getStatus());
+                List<InfoEntity> StatusItems = doc.getDevices(storage);
+                System.out.println("Items(ID,Name,Type, Status):");
+                for (InfoEntity item : StatusItems) {
+                    System.out.println(item.getEqId()+ "   " +  item .getEqName() + "   " + item.getEqType()
+                            + "  " + item.getEqStatus());
                 }
                 break;
             case "4":
             case "Show all reservations of a given equipment":
-                showArchiveEntries();
+                System.out.println("Which device would you like to see reservations for? Enter ID.");
+                String deviceID = scanner.nextLine();
+                List<ReservationsEntity>ReservationsForDevice = doc.getReservationsForItem(deviceID, reservations);
+                System.out.println("DeviceID, Reservation Start, Reservation End, Reserver");
+                for (ReservationsEntity reservation : ReservationsForDevice) {
+                    System.out.println(reservation.getEquipmentId() + "   " +  reservation.getStartDate() + "   " + reservation.getEndDate()
+                            + "  " + reservation.getReserver());
+                }
+                break;
         }
     }
 
@@ -82,10 +88,10 @@ public class DoctorApp {
         if (storage.getAllEquipment().isEmpty()) {
             System.out.println("Storage is empty.");
         } else {
-            List<DoctorViewItem> items = doc.getAllItems(viewStorage);
+            List<InfoEntity> items = doc.getDevices(storage);
             System.out.println("ID  Name   Type");
-            for (DoctorViewItem item : items) {
-                System.out.println(item.getId() + "   " + item.getName() + "   " + item.getType() + "  ");
+            for (InfoEntity item : items) {
+                System.out.println(item.getEqId() + "   " + item.getEqName() + "   " + item.getEqType() + "  ");
             }
             System.out.println("Which device would you like to reserve? Enter ID.");
             int deviceID = Integer.parseInt(scanner.nextLine());
@@ -94,123 +100,6 @@ public class DoctorApp {
             System.out.println("End of reservation date in a (yyyy-mm-dd) format) ");
             var reservationEnd=  Date.valueOf(scanner.nextLine());
             doc.reserveDevice(deviceID, reservationStart, reservationEnd, storage);
-        }
-    }
-
-    private void useDevice() {
-        List<InfoEntity> servicedDevices = tech.getServicedDevices(storage);
-        if (servicedDevices.isEmpty()) {
-            System.out.println("No device is being serviced.");
-        } else {
-            System.out.println("On which device do you wish to end service?");
-            System.out.println("ID  Name       Type");
-            String leftAlignment = "(%-1s) %-10s %-5s %n";
-            for (InfoEntity e : servicedDevices) {
-                System.out.format(leftAlignment, e.getEqId(), e.getEqName(), e.getEqType());
-            }
-            int deviceNumber = Integer.parseInt(scanner.nextLine());
-            tech.endServiceOnDevice(deviceNumber, storage);
-            System.out.println("Enter your service description.");
-            var description = scanner.nextLine();
-            tech.sendToServiceArchives(deviceNumber, description, archives, storage);
-        }
-    }
-
-    private String chooseFilterOption() {
-        System.out.println("Which filter do you want to use?");
-        System.out.println("(1) Show only ECG type devices");
-        System.out.println("(2) Show only EMG type devices");
-        System.out.println("(3) Show only RTG type devices");
-        System.out.println("(4) Show only MRI type devices");
-        System.out.println("(5) Show devices with service expiring in 2 weeks");
-        System.out.println("(6) Show devices with service expiring in 1 month");
-        System.out.println("(7) Show devices with service expiring in half a year");
-        System.out.println("(8) Show devices with expired service");
-
-        return scanner.nextLine();
-    }
-
-    private void performFilter(String option) {
-        List<TechnicianViewItem> items = tech.getAllItems(viewStorage);
-        List<TechnicianViewItem> filteredItems;
-        Calendar cal = Calendar.getInstance();
-        Calendar cal2 = Calendar.getInstance();
-        switch (option) {
-            case "1":
-            case "Show only ECG type devices":
-                filteredItems = items.stream().filter(p -> p.getType().name().equalsIgnoreCase("ecg")).collect(Collectors.toList());
-                break;
-            case "2":
-            case "Show only EMG type devices":
-                filteredItems = items.stream().filter(p -> p.getType().name().equalsIgnoreCase("emg")).collect(Collectors.toList());
-                break;
-            case "3":
-            case "Show only RTG type devices":
-                filteredItems = items.stream().filter(p -> p.getType().name().equalsIgnoreCase("rtg")).collect(Collectors.toList());
-                break;
-            case "4":
-            case "Show only MRI type devices":
-                filteredItems = items.stream().filter(p -> p.getType().name().equalsIgnoreCase("mri")).collect(Collectors.toList());
-                break;
-            case "5":
-            case "Show devices with service expiring in 2 weeks":
-                cal.add(Calendar.DATE, 14);
-                filteredItems = items.stream().filter(p -> !p.getNextServiceDate().after(cal.getTime())).
-                        filter(p -> p.getNextServiceDate().after(cal2.getTime())).collect(Collectors.toList());
-                break;
-            case "6":
-            case "Show devices with service expiring in 1 month":
-                cal.add(Calendar.DATE, 30);
-                filteredItems = items.stream().filter(p -> !p.getNextServiceDate().after(cal.getTime())).
-                        filter(p -> p.getNextServiceDate().after(cal2.getTime())).collect(Collectors.toList());
-                break;
-            case "7":
-            case "Show devices with service expiring in half a year":
-                cal.add(Calendar.DATE, 182);
-                filteredItems = items.stream().filter(p ->  p.getNextServiceDate().before(cal.getTime())).
-                        filter(p -> p.getNextServiceDate().after(cal2.getTime())).collect(Collectors.toList());
-                break;
-            case "8":
-            case "Show devices with expired service":
-                filteredItems = items.stream().filter(p -> p.getNextServiceDate().before(cal2.getTime())).collect(Collectors.toList());
-                break;
-            default:
-                filteredItems = null;
-                break;
-        }
-        if (filteredItems.isEmpty()) {
-            System.out.println("There are not any searched devices.");
-        }
-        else {
-            System.out.println("Name       Type    Next service date");
-            for (TechnicianViewItem item : filteredItems) {
-                String leftAlignment = "%-10s %-5s %-19s %n";
-                System.out.format(leftAlignment, item.getName(), item.getType(), item.getNextServiceDate());
-            }
-        }
-    }
-
-    private void addEquipment() {
-        System.out.println("Enter equipment name:");
-        var name = scanner.nextLine();
-        System.out.println("Enter equipment type:");
-        var type = scanner.nextLine().toUpperCase();
-        System.out.println("Enter production date:");
-        var productionDate = scanner.nextLine();
-        System.out.println("Enter last service date:");
-        var serviceDate = scanner.nextLine();
-        System.out.println("Enter service validity period:");
-        var validity = scanner.nextShort();
-        tech.addEquipment(new Equipment(name, EqType.valueOf(type), Date.valueOf(productionDate),
-                Date.valueOf(serviceDate), EqStatus.valueOf("FREE"), validity), storage);
-    }
-
-    private void showArchiveEntries() {
-        List<ArchiveEntry> entries = tech.getAllArchivesEntries(archives);
-        System.out.println("Service date   EquipmentID   Technician user  Description");
-        for (ArchiveEntry entry : entries) {
-            String leftAlignment = "%-14s %-13s %-16s %-20s %n";
-            System.out.format(leftAlignment, entry.getServiceDate(), entry.getEquipmentId(), entry.getTechnician(), entry.getDescription());
         }
     }
 }
--- src/main/java/service/Doctor.java
+++ src/main/java/service/Doctor.java
@@ -2,14 +2,16 @@ package service;
 
 import persistance.archives.IArchives;
 import persistance.entity.InfoEntity;
+import persistance.entity.ReservationsEntity;
 import persistance.entity.ServiceArchivesEntity;
 import persistance.equipment.IEquipmentStorage;
+import persistance.reservations.IReservationsRepository;
 import persistance.view.DoctorView;
-import persistance.view.DoctorViewStorage;
 import types.Department;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,19 +29,25 @@ public class Doctor extends Staff {
         return new DoctorViewItem(view.getEqID(), view.getEqName(), view.getEqType() );
     }
 
-    public List<DoctorViewItem> getAllItems(DoctorViewStorage storage) {
-        return storage.getAllItems().stream().map(this::mapDoctorViewToItems).collect(Collectors.toList());
+    public List<InfoEntity> getDevices(IEquipmentStorage storage) {
+        return new ArrayList<>(storage.getAllEquipment());
     }
 
-    public List<InfoEntity> getDevicesToReserve(IEquipmentStorage storage) {
+    public List<ReservationsEntity> getReservations(IReservationsRepository reservationsRepository) {
+        return new ArrayList<>(reservationsRepository.getAllReservations());
+    }
+
+    public List<ReservationsEntity> getReservationsForItem(String itemID, IReservationsRepository reservationsRepository) {
+        Integer IntItemID = Integer.parseInt(itemID);
+        return reservationsRepository.getAllReservations().stream().filter(value -> value.getReservationId() == IntItemID).collect(Collectors.toList());
+    }
+    public List<InfoEntity> getDevicesToService(IEquipmentStorage storage) {
         return storage.getAllEquipment().stream().
                 filter(e -> e.getEqStatus().name().equalsIgnoreCase("FREE")).collect(Collectors.toList());
     }
-
 
     public List<InfoEntity> getAvailableDevices(IEquipmentStorage storage) {
-        return storage.getAllEquipment().stream().
-                filter(e -> e.getEqStatus().name().equalsIgnoreCase("FREE")).collect(Collectors.toList());
+        return storage.getAllEquipment().stream()
     }
 
 
@@ -49,48 +57,5 @@ public class Doctor extends Staff {
     public void reserveDevice(Integer deviceID, Date reservationStart, Date reservationEnd, IEquipmentStorage storage) {
         InfoEntity infoEntity = storage.getOneEquipment(deviceID);
         storage.updateEquipmentStatus(id, "OCCUPIED");
-    }
-
-    public List<InfoEntity> getServicedDevices(IEquipmentStorage storage) {
-        return storage.getAllEquipment().stream().
-                filter(e -> e.getEqStatus().name().equalsIgnoreCase("OCCUPIED")).collect(Collectors.toList());
-    }
-
-    public void endServiceOnDevice(int id, IEquipmentStorage storage) {
-        InfoEntity infoEntity = storage.getOneEquipment(id);
-        storage.updateEquipmentStatus(id, "FREE");
-        storage.updateEquipmentServiceDate(id, LocalDate.now());
-    }
-
-    private ServiceArchivesEntity mapArchivesEntryToServiceArchivesEntity(ArchiveEntry entry) {
-        var archivesEntity = new ServiceArchivesEntity();
-        archivesEntity.setServiceDate(entry.getServiceDate());
-        archivesEntity.setEquipmentId(entry.getEquipmentId());
-        archivesEntity.setTechnician(entry.getTechnician());
-        archivesEntity.setDescription(entry.getDescription());
-        return archivesEntity;
-    }
-
-    private ArchiveEntry mapServiceArchivesEntityToArchiveEntry(ServiceArchivesEntity archivesEntity) {
-        if (archivesEntity == null) {
-            return null;
-        }
-        return new ArchiveEntry(archivesEntity.getServiceDate(), archivesEntity.getEquipmentId(), archivesEntity.getTechnician(),
-                archivesEntity.getDescription());
-    }
-
-    public List<ArchiveEntry> getAllArchivesEntries(IArchives archives) {
-        return archives.getAllServices().stream().map(this::mapServiceArchivesEntityToArchiveEntry).collect(Collectors.toList());
-    }
-
-    public void sendToServiceArchives(int id, String description, IArchives archives, IEquipmentStorage storage) {
-        ServiceArchivesEntity archivesEntity = new ServiceArchivesEntity();
-        Calendar calendar = Calendar.getInstance();
-        archivesEntity.setServiceDate(Date.valueOf(LocalDate.now()));
-        archivesEntity.setTechnician(this.getLogin());
-        archivesEntity.setDescription(description);
-        InfoEntity infoEntity = storage.getOneEquipment(id);
-        archivesEntity.setInfoByEquipmentId(infoEntity);
-        archives.addToArchives(archivesEntity);
     }
 }
